0012362c5f67dc99463833774005d61089466b64
szakynr <szakynr@gmail.com>
2016-09-29 18:50:28 +0700

    Revert "pengurangan.py"
    This reverts commit 67ebbdf94915a7ad6b6f9b18873539b5775e1948.

--- pengurangan.py
+++
@@ -1,7 +0,0 @@
-print("PENGURANGAN DUA BUAH BILANGAN")
-print("=============================")
-x = float(input("Bilangan pertama: "))
-y = float(input("Bilangan kedua: "))
-z = x - y
-print("--------------------------")
-print ("Jumlah = ", x - y)
--- perkalian.py
+++
@@ -1,7 +0,0 @@
-print("PERKALIAN DUA BUAH BILANGAN")
-print("=============================")
-x = float(input("Bilangan pertama: "))
-y = float(input("Bilangan kedua: "))
-z = x * y
-print("--------------------------")
-print ("Jumlah = ", x * y)
