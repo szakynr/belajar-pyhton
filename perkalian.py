67ebbdf94915a7ad6b6f9b18873539b5775e1948
szakynr <szakynr@gmail.com>
2016-09-29 18:48:31 +0700

    pengurangan.py
    ini adalah program pengulangan menggunakan python

---
+++ perkalian.py
@@ -0,0 +1,7 @@
+print("PERKALIAN DUA BUAH BILANGAN")
+print("=============================")
+x = float(input("Bilangan pertama: "))
+y = float(input("Bilangan kedua: "))
+z = x * y
+print("--------------------------")
+print ("Jumlah = ", x * y)
